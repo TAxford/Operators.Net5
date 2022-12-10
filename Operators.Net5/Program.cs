acb194b21d65fb936b421f39d6b74ff032833915
Timothy Axford <t.axford2@yahoo.com>
2022-12-09 22:05:54 -0500

    Add project files.

---
+++ Operators.Net5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Operators.Net5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 3;
+            int num3;
+
+
+            //unary operators
+
+
+
+
+        }
+    }
+}
